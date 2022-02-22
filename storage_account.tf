355d3c39053dbdbf56da16e78cc8327dcdc4b496
murggu <amurguzur@gmail.com>
2022-02-22 15:19:50 +0100

    Disable pdnsz creation

--- storage_account.tf
+++ storage_account.tf
@@ -13,34 +13,34 @@ module "storage_account" {
   hns_enabled                         = true
   firewall_bypass                     = ["None"]
   firewall_virtual_network_subnet_ids = []
-  private_dns_zone_ids_blob           = [azurerm_private_dns_zone.sa_zone_blob.id]
-  private_dns_zone_ids_dfs            = [azurerm_private_dns_zone.sa_zone_dfs.id]
+  private_dns_zone_ids_blob           = [azurerm_private_dns_zone.st_zone_blob.id]
+  private_dns_zone_ids_dfs            = [azurerm_private_dns_zone.st_zone_dfs.id]
 }
 
 # DNS Zones
 
 resource "azurerm_private_dns_zone" "st_zone_blob" {
   name                = "privatelink.blob.core.windows.net"
-  resource_group_name = module.resource_group.location
+  resource_group_name = module.resource_group.name
 }
 
 resource "azurerm_private_dns_zone" "st_zone_dfs" {
   name                = "privatelink.dfs.core.windows.net"
-  resource_group_name = module.resource_group.location
+  resource_group_name = module.resource_group.name
 }
 
 # Linking of DNS zones to Virtual Network
 
 resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_blob_link" {
-  name                  = "${var.postfix}_link_blob"
-  resource_group_name   = module.resource_group.location
+  name                  = "${random_string.postfix.result}_link_blob"
+  resource_group_name   = module.resource_group.name
   private_dns_zone_name = azurerm_private_dns_zone.st_zone_blob.name
   virtual_network_id    = module.virtual_network.id
 }
 
 resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_dfs_link" {
-  name                  = "${var.postfix}_link_dfs"
-  resource_group_name   = module.resource_group.location
+  name                  = "${random_string.postfix.result}_link_dfs"
+  resource_group_name   = module.resource_group.name
   private_dns_zone_name = azurerm_private_dns_zone.st_zone_dfs.name
   virtual_network_id    = module.virtual_network.id
 }
