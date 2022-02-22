c069449be2cd76e96d9cb1564533cf74ab27d594
murggu <amurguzur@gmail.com>
2022-02-22 14:46:45 +0100

    Disable pdnsz creation

--- storage_account.tf
+++ storage_account.tf
@@ -7,7 +7,40 @@ module "storage_account" {
   prefix  = var.prefix
   postfix = random_string.postfix.result
 
-  vnet_id     = module.virtual_network.id
-  subnet_id   = azurerm_subnet.default_subnet.id
-  hns_enabled = true
+  vnet_id   = module.virtual_network.id
+  subnet_id = azurerm_subnet.default_subnet.id
+
+  hns_enabled                         = true
+  firewall_bypass                     = ["None"]
+  firewall_virtual_network_subnet_ids = []
+  private_dns_zone_ids_blob           = [azurerm_private_dns_zone.sa_zone_blob.id]
+  private_dns_zone_ids_dfs            = [azurerm_private_dns_zone.sa_zone_dfs.id]
+}
+
+# DNS Zones
+
+resource "azurerm_private_dns_zone" "st_zone_blob" {
+  name                = "privatelink.blob.core.windows.net"
+  resource_group_name = module.resource_group.location
+}
+
+resource "azurerm_private_dns_zone" "st_zone_dfs" {
+  name                = "privatelink.dfs.core.windows.net"
+  resource_group_name = module.resource_group.location
+}
+
+# Linking of DNS zones to Virtual Network
+
+resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_blob_link" {
+  name                  = "${var.postfix}_link_blob"
+  resource_group_name   = module.resource_group.location
+  private_dns_zone_name = azurerm_private_dns_zone.st_zone_blob.name
+  virtual_network_id    = module.virtual_network.id
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "st_zone_dfs_link" {
+  name                  = "${var.postfix}_link_dfs"
+  resource_group_name   = module.resource_group.location
+  private_dns_zone_name = azurerm_private_dns_zone.st_zone_dfs.name
+  virtual_network_id    = module.virtual_network.id
 }
